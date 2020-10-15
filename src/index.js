5aa71ddd42585dc372b1384c8e5bc9ed23ce73c8
dispet <liftesk.@gmail.com>
2020-10-15 18:02:15 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    return expr.match(/.{1,10}/g).map(item => item ===  '**********' ? ' ' : MORSE_TABLE[item.replace(/10/g,'.').replace(/11/g,'-').replace(/0/g, '')]).join('');
 }
 
 module.exports = {
